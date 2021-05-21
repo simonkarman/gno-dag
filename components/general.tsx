83edef2daabfb661d6107568ffa304f4d9f55017
Simon Karman <mail@simonkarman.nl>
2021-05-21 19:59:22 +0200

    Added styled components

---
+++ components/general.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const selectionColor = '#0070f3';
+
+export const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Main = styled.main`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+`;
+
+export const Description = styled.p`
+  line-height: 1.5;
+  font-size: 1.5rem;
+`;
+
+export const Grid = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 900px;
+  margin-top: 3rem;
+
+  @media (max-width: 1000px) {
+    width: 90%;
+  }
+`;
+
+export const Card = styled.div`
+  margin: 1rem;
+  padding: 1.5rem;
+  text-align: left;
+  color: inherit;
+  text-decoration: none;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.25s ease, border-color 0.25s ease;
+  width: 100%;
+
+  :hover, :focus, :active {
+    color: ${selectionColor};
+    border-color: #b8b8b8;
+  }
+
+  h2 {
+    margin: 0 0 1rem 0;
+    font-size: 1.5rem;
+  }
+
+  p {
+    margin: 0;
+    font-size: 1.25rem;
+    line-height: 1.5;
+  }
+`;
