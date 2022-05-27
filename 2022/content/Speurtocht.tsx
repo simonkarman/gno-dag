b8e35ca8b5bd78b312e3839b8e53edb0104c53d7
Simon Karman <mail@simonkarman.nl>
2022-05-28 00:25:01 +0200

    Added speurtocht activiteit

--- 2022/content/Speurtocht.tsx
+++ 2022/content/Speurtocht.tsx
@@ -1,64 +1,92 @@
 import { InputContainer } from '../components/InputContainer';
 import { Story } from '../components/Story';
 import { useLocalStorage } from '../hooks';
+import { useLocalStorageBoolean } from '../hooks/common/useTransformedStorage';
 
 const C01 = <p className='paragraph'>
-  Wolf Wouter
-  Nieuwsgierigheid kent geen tijd.
-  Frisse lucht en beweging is goed.
-  Op jacht.
-  Ollie vergeten waar gympies zijn, op zoek waar die zijn gebleven
-  Speurtocht voor de hond (reuk grapje met stinkschoenen)
-  Iets over olifanten met gympies (voor mama)
+  Hoi allemaal! Ik ben <b>Wouter</b> de Wolf.
+  Wat leuk jullie allemaal te zien.
+  Hoe gaat het nu met jou <b>Ollie</b>?
+  Heb je al weer een deel van je herinnering terug?
 </p>;
 
 const C02 = <p className='paragraph'>
-  Tekst deel 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Hey <b>Wouter</b>!
+  Het gaat al wel iets beter.
+  Maar nog niet al mijn herinnering zijn terug.
+  Ik weet namelijk niet waar mijn gympies zijn.
+  Kan jij ons verder helpen?
 </p>;
 
-const C03 = <p className='paragraph'>
-  Tekst deel 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-</p>;
+const C03 = <>
+  <p className='paragraph'>
+    Uiteraard!
+    Als wolf heb ik een hele goede reuk, dan kan ik ze snel terug vinden.
+    Alhoewel je geen wolf hoeft te zijn om jouw gympies van 5km afstand te ruiken.
+    Maar goed, loop maar mee, dan gaan we ze zoeken.
+    En blijf goed bij elkaar.
+    Als wolf wordt ik onrustig als de roedel zich opsplitst.
+  </p>
+</>;
 
 const C04 = <p className='paragraph'>
-  Tekst deel 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Oh, wat leuk!
+  Ik wist wel dat we gingen wandelen vandaag.
+  Ik heb speciaal daarvoor aan Simon wat leuks voor onderweg meegegeven.
+  Laten we gaan.
+</p>;
+
+const C05 = <p className='paragraph'>
+  Ik heb ook nog iets voorbereid, <b>Ollie</b>!
+  Dus, let tijdens de wandeling goed op.
+  Na de wandeling zullen jullie een raadsel moeten beantwoorden.
+  Lees dus pas verder <b>NA</b> de wandeling.
 </p>;
 
 const Puzzel = () => {
+  const [show, setShow] = useLocalStorageBoolean('speurtocht-show', false);
   const [answer, setAnswer] = useLocalStorage<string>('speurtocht--answer', '');
   const isCorrect = answer === 'bushalte';
   return <>
     <h2>Puzzel</h2>
     <p className='paragraph'>
-      Let tijdens de wandeling goed op.
-      (Black Storie en enkele raadsels)
-      Na de wandeling moeten jullie een vraag beantwoorden.
-      Onderstaande mag pas zichtbaar zijn na de wandeling.
-      We zijn hier drie keer langs gelopen. Het heeft een dak. Maar er woont niemand.
+      Toon onderstaande puzzel pas na de wandeling.
     </p>
     <InputContainer>
-      <input
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value.toLowerCase().trim())}
-        className={isCorrect ? 'correct' : (answer.length > 0 ? 'incorrect' : '')}
-      />
+      {!show && <button onClick={() => setShow(true)}>Toon puzzel</button>}
+      {show && <p className='paragraph clickable' onClick={() => setShow(false)}>Verberg puzzel</p>}
     </InputContainer>
-    {isCorrect && <p className='paragraph'>
-        Inderdaad, een bushalte!
-    </p>}
+    {show && (<>
+      <p className='paragraph'>
+        We zijn hier drie keer langs gelopen.
+        Het heeft een dak.
+        Er woont niemand.
+      </p>
+      <InputContainer>
+        <input
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value.toLowerCase().trim())}
+          className={isCorrect ? 'correct' : (answer.length > 0 ? 'incorrect' : '')}
+        />
+      </InputContainer>
+      {isCorrect && <p className='paragraph'>
+          Inderdaad, een bushalte!
+      </p>}
+    </>)}
   </>;
 };
 export function Speurtocht() {
   return (
     <>
       <p className='paragraph'>
-        Tijd voor een speurtocht!
+        Tijd om die beentjes te strekken.
       </p>
       <Story shortName={'speurtocht'} Puzzle={<Puzzel />} sections={[
-        { title: '1', animalName: 'dog', Component: C01 },
-        { title: '2', animalName: 'elephant', Component: C02 },
-        { title: '3', animalName: 'elephant', Component: C03 },
-        { title: '4', animalName: 'dog', Component: C04 },
+        { title: 'Hoe gaat het?', animalName: 'dog', Component: C01 },
+        { title: 'Zoeken nie', animalName: 'elephant', Component: C02 },
+        { title: 'Roedel', animalName: 'dog', Component: C03 },
+        { title: 'Onderweg', animalName: 'elephant', Component: C04 },
+        { title: 'Roedel', animalName: 'dog', Component: C05 },
       ]} />
     </>
   );
