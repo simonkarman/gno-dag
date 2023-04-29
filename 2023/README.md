7dff7049f8df631d0aaf252ee43859beee8322e6
Simon Karman <mail@simonkarman.nl>
2023-04-29 17:33:28 +0200

    Initial setup of 2023

---
+++ 2023/README.md
@@ -0,0 +1,2 @@
+# GNO Dag 2023
+The GNO (gender neutrale ouderdag) website for edition 2023.
---
+++ 2023/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './assets/key.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          GNO Dag 2023, coming soon!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
