12baf420a3118421a11c97b55f767a59ed17dd8b
Simon Karman <mail@simonkarman.nl>
2023-04-29 19:31:11 +0200

    Use random svg on start

--- 2023/src/pages/index.tsx
+++ 2023/src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Head from 'next/head';
-import keySvg from './assets/key.svg';
+import {useMemo} from 'react';
+import scrollSvg from './assets/scroll.svg';
+import rumSvg from './assets/rum.svg';
+import parrotSvg from './assets/parrot.svg';
 import Image from 'next/image';
 
 export default function Home() {
+  const svg = useMemo(() => {
+    const svgs = [scrollSvg, rumSvg, parrotSvg];
+    return svgs[Math.floor(Math.random() * svgs.length)];
+  }, []);
   return (
     <>
       <Head>
@@ -14,13 +21,13 @@ export default function Home() {
           <h1 className="text-4xl font-bold text-white text-center">GNO Dag 2023</h1>
         </header>
 
-        <main className="bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+        <main className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
           <p className="text-center">
-            Het is bijna weer zo ver! Op zaterdag 6 mei vieren we GNO Dag 2023. Kan je al raden wat het thema dit jaar gaat zijn?
+            Het is bijna zo ver! Op zaterdag 6 mei vieren we GNO Dag 2023. Kan je al raden wat het thema dit jaar gaat zijn?
           </p>
           <hr className="my-4" />
           <div className="w-36 h-36 mx-auto my-10">
-            <Image src={keySvg} className="animate-spin-slow" alt="a pirate like key logo that is spinning" />
+            <Image src={svg} className="animate-spin-slow" alt="a pirate logo that is spinning" />
           </div>
           <hr className="my-4" />
           <p className="text-center">
