ac02669c22264b103d19cf0343c8fbeca19985ae
Simon Karman <mail@simonkarman.nl>
2023-04-29 19:23:50 +0200

    Remove faulty index.html page

--- 2023/src/pages/_document.tsx
+++ 2023/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>GNO Dag 2023</title>
+        <meta property="og:title" content="GNO Dag 2023" key="title" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- 2023/src/pages/index.tsx
+++ 2023/src/pages/index.tsx
@@ -3,26 +3,28 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
-      <header className="max-w-lg mx-auto">
-        <h1 className="text-4xl font-bold text-white text-center">GNO Dag 2023</h1>
-      </header>
+    <>
+      <div className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
+        <header className="max-w-lg mx-auto">
+          <h1 className="text-4xl font-bold text-white text-center">GNO Dag 2023</h1>
+        </header>
 
-      <main className="bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-        <p className="text-center">
-          Het is bijna weer zo ver! Op zaterdag 6 mei vieren we GNO Dag 2023. Kan je al raden wat het thema dit jaar gaat zijn?
-        </p>
-        <hr className="my-4" />
-        <div className="w-36 h-36 mx-auto my-10">
-          <Image src={keySvg} className="animate-spin-slow" alt="a pirate like key logo that is spinning" />
-        </div>
-        <hr className="my-4" />
-        <p className="text-center">
-          Kan je niet wachten? Kijk dan nog eens terug naar
-          {' '}
-          <a className="underline text-gray-600 hover:text-gray-800 visited:text-gray-600" href="https://gno-2022.karman.dev">wat we in 2022 gedaan hebben</a>.
-        </p>
-      </main>
-    </div>
+        <main className="bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+          <p className="text-center">
+            Het is bijna weer zo ver! Op zaterdag 6 mei vieren we GNO Dag 2023. Kan je al raden wat het thema dit jaar gaat zijn?
+          </p>
+          <hr className="my-4" />
+          <div className="w-36 h-36 mx-auto my-10">
+            <Image src={keySvg} className="animate-spin-slow" alt="a pirate like key logo that is spinning" />
+          </div>
+          <hr className="my-4" />
+          <p className="text-center">
+            Kan je niet wachten? Kijk dan nog eens terug naar
+            {' '}
+            <a className="underline text-gray-600 hover:text-gray-800 visited:text-gray-600" href="https://gno-2022.karman.dev">wat we in 2022 gedaan hebben</a>.
+          </p>
+        </main>
+      </div>
+    </>
   )
 }
