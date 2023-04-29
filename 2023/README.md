0562af663e797314adebfc189bbb94759b5eafdf
Simon Karman <mail@simonkarman.nl>
2023-04-29 17:47:04 +0200

    Added reference to GNO Dag 2022

--- 2023/README.md
+++ 2023/README.md
@@ -1,2 +1,4 @@
 # GNO Dag 2023
+[![Netlify Status](https://api.netlify.com/api/v1/badges/9eb01045-ccf2-4472-be75-12bf05808658/deploy-status)](https://app.netlify.com/sites/gno-dag-2023/deploys)
+
 The GNO (gender neutrale ouderdag) website for edition 2023.
--- 2023/src/App.tsx
+++ 2023/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import logo from './assets/key.svg';
+import key from './assets/key.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={key} className="App-logo" alt="logo" />
         <p>
           GNO Dag 2023, coming soon!
+        </p>
+        <p className="App-small-text">
+          Kan je niet wachten? Kijk dan nog eens terug naar wat we vorig jaar gedaan hebben: ga nu naar <a href="https://gno-dag-2022.karman.dev">GNO Dag 2022</a>.
         </p>
       </header>
     </div>
