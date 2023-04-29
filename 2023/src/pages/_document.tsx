65cf4ca73bb3222ff778a1016477af382833cb79
Simon Karman <mail@simonkarman.nl>
2023-04-29 19:26:17 +0200

    Move title to next/head in Home

--- 2023/src/pages/_document.tsx
+++ 2023/src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>GNO Dag 2023</title>
-        <meta property="og:title" content="GNO Dag 2023" key="title" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
