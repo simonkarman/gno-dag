ea8d2d41c20a27458e399adcb8b31dbf7031887f
Simon Karman <mail@simonkarman.nl>
2025-06-27 10:11:51 +0200

    Updated docs with other sentences

--- 2025/docs/src/app/page.tsx
+++ 2025/docs/src/app/page.tsx
@@ -13,9 +13,9 @@ function Blank(props: { short?: boolean, multiline?: boolean, text?: string }) {
 }
 
 function Card(props: { title: string, children?: React.ReactNode }) {
-  return <div className="relative border rounded-lg w-80 break-inside-avoid-page">
-    <span className="absolute left-4 top-4 w-3 h-3 bg-purple-800 rounded-full"></span>
-    <span className="absolute right-4 top-4 w-3 h-3 bg-purple-800 rounded-full"></span>
+  return <div className="relative border rounded-lg w-80 break-inside-avoid-page text-justify">
+    <span className="absolute left-4 top-4 w-3 h-3 bg-gray-600 rounded-full"></span>
+    {/*<span className="absolute right-4 top-4 w-3 h-3 bg-purple-800 rounded-full"></span>*/}
     <div className='p-3 space-y-4'>
       <h2 className="font-bold border-b border-gray-200 pb-1 mb-3 text-center">{props.title}</h2>
       {props.children}
@@ -24,29 +24,30 @@ function Card(props: { title: string, children?: React.ReactNode }) {
   </div>;
 }
 
+// Hint: Bewaarde Opgeslagen Momenten
+
 const attendees = ['Govie', 'Jac.', 'Simon', 'Lisa', 'Marjolein', 'Tim'];
 const cardTypes = [
-  'Ik wens jou',
-  'Als ik aan jou denk, dan denk ik aan',
-  'Jij bent goed in',
-  'Als ik een eigenschap met jou zou willen ruilen dan zou ik',
-  'Wat ik aan jouw bewonder is',
+  'Afgelopen jaar heb jij',
+  'Je gebruikt vaak het woord',
+  'Je kunt geen dag zonder',
+  'Jouw go-to snack is',
+  'Je reageert op berichten altijd',
 ];
 
 export default function Home() {
   return (
     <div className="flex items-start flex-wrap gap-3">
-      {attendees.map(attendee => (<Card key={attendee} title={attendee}>
-        <p><Blank short />. Ik ben <Blank short /> jaar oud en ik woon in <Blank />.</p>
-        <p>Als ik mezelf zou omschrijven in drie woorden, dan zouden dat zijn: <Blank />, <Blank /> en <Blank />.</p>
-        <p>Op een vrije zaterdag doe ik het liefste <Blank multiline /></p>
+      {attendees.map(attendee => (<Card key={attendee} title={'Van ' + attendee + ' aan ' + attendee}>
+        <p>Als ik thuis kom, doe ik het liefste <Blank multiline /> en momenteel ben ik helemaal weg van <Blank multiline /></p>
+        <p>Mijn favorite app op dit moment is <Blank /> en ik ben het gelukkigste als <Blank multiline /></p>
       </Card>))}
-      {attendees.flatMap(a => attendees.filter(b => a !== b).map((b, i) => <Card key={a + b + i} title={`${a} -> ${b}`}>
-        <p>{cardTypes[i]} <Blank multiline /></p>
-      </Card> ))}
-      {attendees.map(attendee => <Card key={attendee} title={attendee + ' aan ' + attendee} >
+      {attendees.map(attendee => <Card key={attendee + '2'} title={'Van ' + attendee + ' aan ' + attendee} >
         <p>Over 5 jaar hoop ik <Blank multiline /></p>
       </Card>)}
+      {attendees.flatMap((a, ai) => attendees.filter(b => a !== b).map((b, bi) => <Card key={cardTypes[(ai + bi + 1) % cardTypes.length] + a + b + bi} title={`Van ${a} aan ${b}`}>
+        <p>{cardTypes[(ai + bi + 1) % cardTypes.length]} <Blank multiline /></p>
+      </Card> )).sort(((a, b) => a.key?.localeCompare(b.key ?? '') || 0))}
     </div>
   );
 }
